cb789a25c396b3f54eec138251c8decfa6c5af76
neil patel <neil@myindianweddingapp.com>
2014-05-16 21:15:42 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+== README
+
+Etsy demo application
+
+* Ruby 2.0
+
+* Rails 4.0.4
+
+* Bootstrap 3.0
+
+* Dropbox Api
+
+* Prodution - Postgres
+
+* Development - SQL
+
+
+Please feel free to use a different markup language if you do not plan to run
+<tt>rake doc:app</tt>.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Etsydemo2014::Application.config.secret_key_base = '2135b53c68106dd7d89e98624f862dff4a3d4dd440c472aea0cf519fad3da4c46a53c609ef7983c105da67092ee15b8ece689218d6e4a4613b8faace522932c5'
