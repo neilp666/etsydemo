963ed647e961acc950756fd618df36f3260ba624
neil patel <neil@myindianweddingapp.com>
2014-05-17 15:19:05 +0100

    Add figaro gem

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,8 @@
 class Listing < ActiveRecord::Base
 
 has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missingimages.jpg"
-validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
+validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png),
+
+:storage => :dropbox,
+:dropbox_credentials => Rails.root.join("config/dropbox.yml")
 end
