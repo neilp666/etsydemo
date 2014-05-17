7b3f4f29ce07bd5d06f35121ab8e9737ba29af9b
neil patel <neil@myindianweddingapp.com>
2014-05-17 21:23:40 +0100

    Add Seller Page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Etsydemo2014::Application.routes.draw do
   get "pages/about"
   get "pages/contact"
 
+  get  "seller" => "listings#seller"
+
   root 'listings#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
