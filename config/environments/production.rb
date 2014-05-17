67944ef60b263324c734c01950587911ac04c714
neil patel <neil@myindianweddingapp.com>
2014-05-17 16:46:02 +0100

    Install and set up Devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Etsydemo2014::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { host: 'etsy2014demo.herokuapp.com' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Etsydemo2014::Application.routes.draw do
+  devise_for :users
   resources :listings
 
   get "pages/about"
