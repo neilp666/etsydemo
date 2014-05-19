acef8f5130f6505a44b0affe0c6b6a22e67c1b71
neil patel <neil@myindianweddingapp.com>
2014-05-19 20:08:55 +0100

    Changed listing file

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,19 +1,17 @@
 class Listing < ActiveRecord::Base
-  if Rails.env.development?
 
-    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
-    validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
-
-  else
-
-
-  	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
-	validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png), 
-	  :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+        if Rails.env.development?
+            has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.jpg"
+            validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
+        else
+        has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.jpg",
+                          :storage => :dropbox,
+                          :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                          :path => ":style/id_:filename"
+        end
+  
     
-	end
-
+  validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png) 
   validates :name, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates_attachment_presence :image
